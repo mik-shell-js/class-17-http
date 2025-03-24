17b1edc8046739853a2c81b99e3dcde2d68d40d0
Kyle J. Harms <harmsk@users.noreply.github.com>
2025-03-24 15:48:48 +0000

    activity solution

--- server/server.mjs
+++ server/server.mjs
@@ -13,18 +13,49 @@ app.use(express.json());
 // path to the public folder
 const publicPath = path.join(__dirname, 'public');
 
-// TODO: GET /index.html
+// GET /index.html
+app.get(['/', '/index.html'], (request, response) => {
+  response.sendFile(path.join(publicPath, "/index.html"))
+})
+
+app.get('/styles/site.css', (req, res) => {
+  res.sendFile(path.join(publicPath, req.route.path))
+})
+
+app.get('/api/documents.json', (req, res) => {
+  res.json([
+    {
+      name: "Apple",
+      category: "fruit",
+      price: 1.99,
+      quantity: 150,
+      supplier: "Organic Farms Inc",
+      organic: true,
+      country: "USA",
+      expiration: new Date("2023-12-15")
+    },
+    {
+      name: "Banana",
+      category: "fruit",
+      price: 0.59,
+      quantity: 200,
+      supplier: "Tropical Imports",
+      organic: false,
+      country: "Ecuador",
+      expiration: new Date("2023-12-10")
+    }]).status(200)
+})
 
 
-
-// TODO: 404 - not found
-
-
+// 404 - not found
+app.use((req, res, next) => {
+  return res.json({ message: "Not Found", status: 404 }).status(404);
+})
 
 // 500 - Any server error
-// app.use((err, req, res, next) => {
-//   return res.status(500).json({ error: err });
-// });
+app.use((err, req, res, next) => {
+  return res.status(500).json({ error: err });
+});
 
 // start server on port
 app.listen(port, () => {
